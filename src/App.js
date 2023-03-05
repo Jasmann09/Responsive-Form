484014872f50e169803d6f4a22c800114ab494aa
Jasmann09 <js0289@srmist.edu.in>
2023-03-05 12:02:00 +0530

    React : HTML Forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Your name.." />
+
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" placeholder="Your email.." />
+
+        <label for="number">Phone Number:</label>
+        <input type="number" id="phone-number" name="number" placeholder="Your phone number.." />
+
+        <label for="number">Address:</label>
+        <input type="text" id="address" name="address" placeholder="Your address.." />
+
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" placeholder="Write something.." style={{height:"200px"}}></textarea>
+
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
